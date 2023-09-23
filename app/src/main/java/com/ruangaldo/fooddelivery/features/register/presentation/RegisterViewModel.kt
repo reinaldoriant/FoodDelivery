df067f4df8c691f8f4d02e9936153fc4952452ef
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-23 21:54:28 +0700

    + add register view model

---
+++ app/src/main/java/com/ruangaldo/fooddelivery/features/register/presentation/RegisterViewModel.kt
@@ -0,0 +1,40 @@
+package com.ruangaldo.fooddelivery.features.register.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ruangaldo.fooddelivery.features.register.domain.IRegister
+import com.ruangaldo.fooddelivery.features.register.domain.RegisterEntity
+import com.ruangaldo.fooddelivery.features.register.domain.RegisterRequestEntity
+import com.ruangaldo.fooddelivery.shared.domain.ViewResource
+import kotlinx.coroutines.launch
+
+/**
+ * Written with joy and smile by Ruang Aldo on 23/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+class RegisterViewModel constructor(private val register: IRegister) : ViewModel() {
+    val registerUi: LiveData<ViewResource<RegisterEntity>> get() = _registerUi
+    private val _registerUi = MutableLiveData<ViewResource<RegisterEntity>>()
+
+    fun a() {
+        viewModelScope.launch {
+            register.post(
+                RegisterRequestEntity(
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ),
+            ).collect {
+                _registerUi.value = it
+            }
+        }
+    }
+}
