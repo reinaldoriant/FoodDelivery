7c80bcc5100de322a11de35b72302a1f746f2807
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-23 21:15:39 +0700

    + add constant

---
+++ app/src/main/java/com/ruangaldo/fooddelivery/shared/Constant.kt
@@ -0,0 +1,12 @@
+package com.ruangaldo.fooddelivery.shared
+
+/**
+ * Written with joy and smile by Ruang Aldo on 23/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+class Constant {
+    companion object {
+        const val INT_EMPTY = 0
+    }
+}
