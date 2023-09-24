74a75486ad43dc582975b0eaeb943d6e95cfb3aa
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-24 09:42:12 +0700

    + add snackbar ui

---
+++ app/src/main/java/com/ruangaldo/fooddelivery/shared/ui/Snackbar.kt
@@ -0,0 +1,55 @@
+package com.ruangaldo.fooddelivery.shared.ui
+
+import android.content.Context
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.android.material.behavior.SwipeDismissBehavior
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+import com.ruangaldo.fooddelivery.R
+import com.ruangaldo.fooddelivery.shared.domain.InvalidData
+
+/**
+ * Written with joy and smile by Ruang Aldo on 24/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+fun Context.showSnackbar(
+    view: View,
+    type: Throwable
+) {
+    lateinit var mSnackBar: Snackbar
+
+    mSnackBar = Snackbar.make(
+        view,
+        when (type) {
+            InvalidData() -> {
+                "Akun anda telah terdaftar, Silahkan ganti yang lain"
+            }
+
+            else -> {
+                "Periksa konektivitas anda."
+            }
+        },
+        Snackbar.LENGTH_LONG
+    )
+
+    with(mSnackBar) {
+        setBackgroundTint(ContextCompat.getColor(this@showSnackbar, R.color.red_F5))
+
+        setAction("Tutup") {
+            dismiss()
+        }
+
+        view.setPadding(16, 0, 16, 16)
+
+        val behavior = BaseTransientBottomBar.Behavior().apply {
+            setSwipeDirection(SwipeDismissBehavior.SWIPE_DIRECTION_ANY)
+        }
+
+        setBehavior(behavior).animationMode = BaseTransientBottomBar.ANIMATION_MODE_SLIDE
+        setTextColor(ContextCompat.getColor(this@showSnackbar, R.color.black_333))
+        setActionTextColor(ContextCompat.getColor(this@showSnackbar, R.color.red_B74))
+        show()
+    }
+}
