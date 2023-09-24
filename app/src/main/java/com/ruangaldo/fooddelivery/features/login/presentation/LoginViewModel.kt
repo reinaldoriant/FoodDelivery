95593841cf461a363f2eda61aa3251eecaee43b5
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-24 10:34:58 +0700

    + add login view model

---
+++ app/src/main/java/com/ruangaldo/fooddelivery/features/login/presentation/LoginViewModel.kt
@@ -0,0 +1,31 @@
+package com.ruangaldo.fooddelivery.features.login.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ruangaldo.fooddelivery.features.login.domain.ILogin
+import com.ruangaldo.fooddelivery.features.login.domain.LoginEntity
+import com.ruangaldo.fooddelivery.features.login.domain.LoginRequestEntity
+import com.ruangaldo.fooddelivery.shared.domain.ViewResource
+import kotlinx.coroutines.launch
+
+/**
+ * Written with joy and smile by Ruang Aldo on 24/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+class LoginViewModel constructor(private val login: ILogin) : ViewModel() {
+    val loginUi: LiveData<ViewResource<LoginEntity>> get() = _loginUi
+    private val _loginUi = MutableLiveData<ViewResource<LoginEntity>>()
+
+    fun post(request: LoginRequestEntity) {
+        viewModelScope.launch {
+            login.post(
+                request
+            ).collect {
+                _loginUi.value = it
+            }
+        }
+    }
+}
