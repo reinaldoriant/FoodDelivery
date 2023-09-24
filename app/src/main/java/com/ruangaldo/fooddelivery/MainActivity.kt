0414f2f6d190a6dc0cfc4352637948341cd60f02
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-24 09:45:03 +0700

    ! modify main activity

--- app/src/main/java/com/ruangaldo/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/ruangaldo/fooddelivery/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.ruangaldo.fooddelivery
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.ruangaldo.fooddelivery.factories.RegisterViewModelFactory
 import com.ruangaldo.fooddelivery.features.register.presentation.RegisterViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -12,17 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initViewModel()
-        _viewModelRegister.post()
-        _viewModelRegister.registerUi.observe(this) {
-            Log.d("Check", "$it")
-        }
-    }
-
-    private fun initViewModel() {
-        _viewModelRegister = ViewModelProvider(
-            this,
-            RegisterViewModelFactory.FACTORY
-        )[RegisterViewModel::class.java]
     }
 }
