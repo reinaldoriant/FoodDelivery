cc1a396d09083bc72ced58b0fa2f8b9c1aad76d4
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-24 15:04:57 +0700

    + add decorator login and register

---
+++ app/src/main/java/com/ruangaldo/fooddelivery/decorator/LoginCacheDecorator.kt
@@ -0,0 +1,29 @@
+package com.ruangaldo.fooddelivery.decorator
+
+import com.ruangaldo.fooddelivery.features.login.domain.ILogin
+import com.ruangaldo.fooddelivery.features.login.domain.LoginEntity
+import com.ruangaldo.fooddelivery.features.login.domain.LoginRequestEntity
+import com.ruangaldo.fooddelivery.features.user.data.local.toUserInfoEntity
+import com.ruangaldo.fooddelivery.features.user.domain.ISaveUserInfo
+import com.ruangaldo.fooddelivery.shared.domain.ViewResource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+/**
+ * Written with joy and smile by Ruang Aldo on 24/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+class LoginCacheDecorator(
+    private val decorator: ILogin,
+    private val cache: ISaveUserInfo
+) : ILogin {
+    override fun post(request: LoginRequestEntity): Flow<ViewResource<LoginEntity>> = flow {
+        decorator.post(request).collect { response ->
+            if (response is ViewResource.Success) {
+                cache.save(response.data.toUserInfoEntity())
+            }
+            emit(response)
+        }
+    }
+}
---
+++ app/src/main/java/com/ruangaldo/fooddelivery/factories/LoginDecoratorFactory.kt
@@ -0,0 +1,24 @@
+package com.ruangaldo.fooddelivery.factories
+
+import com.ruangaldo.fooddelivery.decorator.LoginCacheDecorator
+import com.ruangaldo.fooddelivery.features.login.domain.ILogin
+import com.ruangaldo.fooddelivery.features.user.domain.ISaveUserInfo
+
+/**
+ * Written with joy and smile by Ruang Aldo on 24/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+class LoginDecoratorFactory {
+    companion object {
+        fun createLoginDecorator(
+            decorator: ILogin,
+            cache: ISaveUserInfo
+        ): ILogin {
+            return LoginCacheDecorator(
+                decorator,
+                cache
+            )
+        }
+    }
+}
