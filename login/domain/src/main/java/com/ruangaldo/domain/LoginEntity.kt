187d048861f0819fd69c91980f49e4c1a6e3829b
Reinaldo Riant <reinaldo.riant@kompas.com>
2023-09-26 12:46:05 +0700

    ! move login domain to module domain

---
+++ login/domain/src/main/java/com/ruangaldo/domain/LoginEntity.kt
@@ -0,0 +1,16 @@
+package com.ruangaldo.domain
+
+/**
+ * Written with joy and smile by Ruang Aldo on 23/09/23.
+ * Github: https://github.com/reinaldoriant
+ */
+
+data class LoginEntity(
+    val token: String,
+    val name: String,
+    val email: String,
+    val address: String,
+    val houseNumber: String,
+    val phoneNumber: String,
+    val city: String
+)
